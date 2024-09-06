17fe1c3e47c426c0170bf3e3ce17fc5d9866a71d
ZORO333 <53059662+ZORO333@users.noreply.github.com>
2024-09-06 09:21:22 +0200

    change 1

---
+++ Quiz1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string Name = "Hadi";
+int Age = 21;
+decimal a = 3.67m;
+float b = 6.6f;
+
+
+
+Console.WriteLine(a*10);
+Console.WriteLine(Name);
